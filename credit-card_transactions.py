439768c5a188e9814c45713ebb9b462191779f78
siozosdr <sokras2008@gmail.com>
2015-09-17 16:12:59 +0200

    1st commit

--- credit-card_transactions.py
+++ credit-card_transactions.py
@@ -1,1 +1,22 @@
 __author__ = 'sokras'
+from __future__ import division
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+def read_file(file):
+    with open(file, 'r') as rows:  # open the file
+        fields = rows.readline()
+        fields = fields.split(',')
+        for row in csv.reader
+        return fields, lines
+
+def process_lines(lines):
+    for l in lines:
+        print a
+def main():
+    fields, lines = read_file("subscription_report.csv")
+    print fields
+    for l in lines:
+        print l
+main()
